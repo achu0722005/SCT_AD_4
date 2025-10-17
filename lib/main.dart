3104745a576d8a5b551194f57ad1628aede8dd06
ACHUXAPPDEV <akshaynv207@gmail.com>
2025-10-17 22:44:41 +0530

    updated code 001

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-void main(){
+void main() {
   runApp(Myapp());
 }
+
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
@@ -24,22 +27,310 @@ class Qrcodehomescreen extends StatefulWidget {
 }
 
 class _QrcodehomescreenState extends State<Qrcodehomescreen> {
+  int _selectedindex = 0;
+  void _onitentappe (int index){
+    setState(() {
+      _selectedindex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Qr scanner / generator",style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-        ),
+        backgroundColor: Color(0xFF2e4a78),
+        title: Text(
+          "Qr scanner / generator",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.cancel,color: Colors.white,size: 10,))
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.cancel, color: Colors.white, size: 20),
+          ),
         ],
         centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 250,
+                color: Colors.pink,
+                child: MobileScanner(
+                  controller: MobileScannerController(detectionSpeed:DetectionSpeed.noDuplicates ),
+                  onDetect: (capture){
+                    final List<Barcode> barcodes = capture.barcodes;
+                    final Uint8List? image = capture.image;
+                    for(final barcode in barcodes){
+                      print("barcode found : ${barcode.rawValue}");
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                height: 600,
+
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 100,
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              print("qr history");
+                            },
+                            splashColor: Colors.blue,
+                            child: Container(
+                              width: 90,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF3c71c7),
+                                borderRadius: BorderRadius.circular(50),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 3,
+                                    blurRadius: 11,
+                                  ),
+                                ],
+                              ),
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.camera,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              print("Add qr code");
+                            },
+                            child: Container(
+                              width: 130,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: Color(0xFFf37c32),
+                                borderRadius: BorderRadius.circular(50),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 3,
+                                    blurRadius: 11,
+                                  ),
+                                ],
+                              ),
+                              child: IconButton(
+                                onPressed: () {
+                                  print("Scan qr");
+                                },
+                                icon: Icon(
+                                  Icons.add_circle_outlined,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              print("clicked");
+                            },
+                            child: Container(
+                              width: 90,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: Color(0xFFa1a5a7),
+                                borderRadius: BorderRadius.circular(50),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 3,
+                                    blurRadius: 11,
+                                  ),
+                                ],
+                              ),
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.access_time_outlined,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        cursorColor: Colors.blue,
+                        decoration: InputDecoration(
+                          hintText: "Enter the text or URL to generate",
+                          border: InputBorder.none,
+                          filled: true,
+                          fillColor: Colors.grey.shade100,
+                          prefixIcon: Icon(Icons.link, color: Colors.black),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 12,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.blue,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red, width: 2),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      height: 300,
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 340,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 11,
+                            )
+                          ]
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              width: 200,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text("Qr generated"),
+                            ),
+                            SizedBox(height: 10),
+
+                            Container(
+                              width: 300,
+
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ElevatedButton.icon(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.download,
+                                      color: Colors.white,
+                                    ),
+                                    label: Text(
+                                      "Save Image",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue,
+                                    ),
+                                  ),
+                                  ElevatedButton.icon(
+                                    onPressed: () {},
+                                    label: Text(
+                                      "Share",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    icon: Icon(
+                                      Icons.share_rounded,
+                                      color: Colors.white,
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: "Scanner",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_2),
+            label: "My codes",
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        currentIndex: _selectedindex,
+        onTap: _onitentappe,
       ),
     );
   }
 }
-
